24a7fffebba5a08b750281e21fbd33a7538a9db1
Ray_Shangguan <a0963382999@gmail.com>
2023-09-06 11:46:50 +0800

    refactor timeout_input_catcher.py slightly

--- windows/timeout_input_catcher.py
+++ windows/timeout_input_catcher.py
@@ -1,23 +1,28 @@
 from inputimeout import inputimeout, TimeoutOccurred
 import sys
 
-
+DEFAULT_TIMEOUT = 15
 def write_to_file(file_name, content):
     try:
         with open(file_name, 'w', encoding='utf-8') as file:
             file.write(content)
-        print(f"Content has been written to {file_name}.")
+        # print(f"Content has been written to {file_name}.")
     except Exception as e:
         print(f"An error occurred while writing to the file: {str(e)}")
 
+def get_user_input(timeout=DEFAULT_TIMEOUT) -> str:
+    try:
+        return inputimeout(prompt="Enter y to continue training,Enter s to stop training:", timeout=timeout)
+    except TimeoutOccurred:
+        return 'timeout'
+    except Exception as e:
+        return "error"
 
 if __name__ == "__main__":
-    timeout = 15
+    timeout = DEFAULT_TIMEOUT
     if len(sys.argv) > 1:
         parameter = sys.argv[1]
         timeout = int(parameter)
-    try:
-        c = inputimeout(prompt="Enter y to continue training,Enter s to stop training:", timeout=timeout)
-    except TimeoutOccurred:
-        c = 'timeout'
-    write_to_file("input.tmp", c)
+        
+    user_input = get_user_input(timeout=timeout)
+    write_to_file("input.tmp", user_input)
